435fea1d6925698b5c83a90fbbd780a3e71337bb
Ali Fazeli <alifazeli1969@gmail.com>
2025-03-12 22:00:21 +0100

    Converted all double quotes to single quotes

--- bg.js
+++ bg.js
@@ -3,14 +3,14 @@ let mainTabId;
 
 browser.runtime.onMessage.addListener(async (message, sender) => {
 	switch (message.type) {
-		case "create_mirror":
+		case 'create_mirror':
 			win = await browser.windows.create({
-				url: "blank.html",
-				type: "normal",
+				url: 'blank.html',
+				type: 'normal',
 				width: 800,
 				height: 600
 			});
-			storage = await browser.storage.local.get("mirrorTabIds");
+			storage = await browser.storage.local.get('mirrorTabIds');
 			mirrorTabIdsArray = storage.mirrorTabIds || [];
 			mirrorTabIdsArray.push(win.tabs[0].id);
 			await browser.storage.local.set({
@@ -19,12 +19,12 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
 			});
 			break;
 		case 'check_if_main_tab':
-			storage = await browser.storage.local.get("mainTabId");
+			storage = await browser.storage.local.get('mainTabId');
 			if (sender.tab.id === storage.mainTabId)
 				return true;
 			else
 				return false;
-		case "update_mirror_tab":
+		case 'update_mirror_tab':
 			browser.scripting.executeScript({
 				target: { tabId: message.mirrorTabId },
 				func: (mainTabChatContent) => {
@@ -37,14 +37,14 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
 				args: [message.mainTabChatContent]
 			});
 			break;
-		case "set_mirror_tabs_scroll_position":
+		case 'set_mirror_tabs_scroll_position':
 			storage = await browser.storage.local.get(
-						["mainTabId", "mirrorTabIds"],
+						['mainTabId', 'mirrorTabIds'],
 					  );
 			mainTabId = storage.mainTabId;
 			mirrorTabIdsArray = storage.mirrorTabIds;
 			if (
-				typeof mainTabId === "undefined" ||
+				typeof mainTabId === 'undefined' ||
 				!sender.tab.id === mainTabId ||
 				!Array.isArray(mirrorTabIdsArray)
 			)
@@ -75,17 +75,17 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
 	mainTabId = storage.mainTabId;
 	mirrorTabIdsArray = storage.mirrorTabIds;
 	if (
-		typeof mainTabId === "undefined" ||
+		typeof mainTabId === 'undefined' ||
 		!Array.isArray(mirrorTabIdsArray) ||
 		tabId == mainTabId ||
 		!mirrorTabIdsArray.includes(tabId)
 	)
 		return;
-	if (changeInfo.status === "complete") {
+	if (changeInfo.status === 'complete') {
 		browser.tabs.sendMessage(
 			storage.mainTabId,
 			{
-				type: "mirror_tab_ready",
+				type: 'mirror_tab_ready',
 				mirrorTabId: tabId
 			}
 		);
@@ -94,12 +94,12 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
 
 browser.tabs.onRemoved.addListener(async (tabId) => {
 	storage = await browser.storage.local.get(
-				["mainTabId", "mirrorTabIds"],
+				['mainTabId', 'mirrorTabIds'],
 			  );
 	mainTabId = storage.mainTabId;
 	mirrorTabIdsArray = storage.mirrorTabIds;
 	if (
-		typeof mainTabId === "undefined" ||
+		typeof mainTabId === 'undefined' ||
 		!Array.isArray(mirrorTabIdsArray)
 	)
 		return;
@@ -107,7 +107,7 @@ browser.tabs.onRemoved.addListener(async (tabId) => {
 		for (let mirrorTabId of mirrorTabIdsArray)
 			await browser.tabs.remove(mirrorTabId);
 		await browser.storage.local.remove(
-			["mainTabId", "mirrorTabIds"]
+			['mainTabId', 'mirrorTabIds']
 		);
 	} else if (mirrorTabIdsArray.includes(tabId)) {
 		mirrorTabIdsArray.forEach((mirrorTabId, index) => {
@@ -116,7 +116,7 @@ browser.tabs.onRemoved.addListener(async (tabId) => {
 		});
 		if (mirrorTabIdsArray.length == 0)
 			await browser.storage.local.remove(
-				["mainTabId", "mirrorTabIds"]
+				['mainTabId', 'mirrorTabIds']
 			);
 		else
 			await browser.storage.local.set({
